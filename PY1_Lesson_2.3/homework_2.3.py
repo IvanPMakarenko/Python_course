77c7145d01996825ed22c98672445608b6a25987
IvanPMakarenko <ivan.p.makarenko@gmail.com>
2017-06-04 21:58:29 +0700

    HW 2.3 с доработками
    HW 2.3 с доработками

--- PY1_Lesson_2.3/homework_2.3.py
+++ PY1_Lesson_2.3/homework_2.3.py
@@ -2,57 +2,64 @@
 import json
 import chardet
 import re
+import collections
 from pprint import pprint
 
 
-def json_loader():
-	user_name = input('Введите название файла из списка newsafr, newscy, newsit, newsfr:')
-	list_name = user_name + '.json'
-	with open(list_name, 'rb') as f:
-		book = f.read()
+def json_loader(user_file):
+	list_name = user_file + '.json'
+	with open(list_name, 'rb') as file:
+		book = file.read()
 		text_format = chardet.detect(book)
 		print('список в кодировке: {}'.format(text_format['encoding']))
-		with open(list_name, encoding = text_format['encoding']) as f:
-			json_book = json.load(f)
+		correct_book = book.decode(text_format['encoding'])
+		json_book = json.loads(correct_book)
 		return json_book
 
-#print (json_loader())
+#pprint (json_loader())
 
-def json_into_list_of_words():
+def json_into_list_of_words(user_file):
 	news_list = []
-	for news in json_loader()['rss']['channel']['item']:
-		try:
-			new = news['description']['__cdata']
-		except:
-			new = re.sub('<br>|\n|\r',' ', re.sub('\.|[,]|[)]|[(]|!|["]','', news['description'])).strip().lower().split(' ')
+	for news in json_loader(user_file)['rss']['channel']['item']:
+		if type(news['description']) == dict:
+			new_without_symb = re.sub('<[^<]+?>', '', news['description']['__cdata']).lower()
+			new_word = re.findall('\w+', new_without_symb)
 		else:
-			new = re.sub('<br>|\n|\r',' ', re.sub('\.|[,]|[)]|[(]|!|["]','', news['description']['__cdata'])).strip().lower().split(' ')
-		news_list.extend(new)
+		 	new_without_symb = re.sub('<[^<]+?>', '', news['description']).lower()
+		 	new_word = re.findall('\w+', new_without_symb)
+		news_list.extend(new_word)
 	return news_list
 
-
+#pprint(json_into_list_of_words())
 
 def top10_words():
+	user_file = input('Введите название файла из списка newsafr, newscy, newsit, newsfr:')
 	user_top_n = int(input('Введите, какое количество популярных слов вас интересует:'))
 	words_dict = {}
-	for word in json_into_list_of_words():
-		if word not in words_dict and len(word) > 2: #условие >2 для того, чтобы отсеять предлоги, тире, дефисы и т.п.
-			words_dict[word] = 1
-		elif word in words_dict:
-			words_dict[word] += 1
-	sorted_words_dict = sorted (words_dict.items(), key = lambda x:x[1], reverse=True)
-	i = 0
-	top_list = []
-	for word in sorted_words_dict:
+	rate_list = collections.Counter(json_into_list_of_words(user_file)).most_common(user_top_n)
+	i=0
+	for word_position in rate_list:
 		i += 1
-		if i <= user_top_n:
-			top_list.append(word[1])
-			print ('{0}. Слово "{1}" встречается в новостях {2} раз'.format(i, word[0].upper(), word[1]))
-		elif word[1] in top_list and i == user_top_n + 1:
-			print ('{0}. Слово "{1}" встречается в новостях {2} раз (делит последнее место)'.format(i, word[0].upper(), word[1]))
-			i -= 1
-		else:
-			break 
+		print('{0}. Слово "{1}" встречается в новостях {2} раз'.format(i, str(word_position[0]).upper(), word_position[1]))
+	# for word in json_into_list_of_words(user_file):
+	# 	if word not in words_dict: 
+	# 		words_dict[word] = 1
+	# 	else:
+	# 		words_dict[word] += 1
+	# sorted_words_dict = sorted (words_dict.items(), key = lambda x:x[1], reverse=True)
+	# i = 0
+	# top_list = []
+	# for word in sorted_words_dict:
+	# 	i += 1
+	# 	if i <= user_top_n:
+	# 		top_list.append(word[1])
+	# 		print ('{0}. Слово "{1}" встречается в новостях {2} раз'.format(i, word[0].upper(), word[1]))
+	# 	elif word[1] in top_list and i == user_top_n + 1:
+	# 		print ('{0}. Слово "{1}" встречается в новостях {2} раз (делит последнее место)'.format(i, word[0].upper(), word[1]))
+	# 		i -= 1
+	# 	else:
+	# 		break 
 
 top10_words()
+
 
